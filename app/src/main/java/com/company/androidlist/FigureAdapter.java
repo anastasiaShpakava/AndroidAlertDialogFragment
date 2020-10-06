5f1b66291a7244a94519d215fbaec8b434544dae
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-06 17:21:58 +0300

    deleted DialogFigureFragment.java, added Dialog object, added layout for alertDialog

--- app/src/main/java/com/company/androidlist/FigureAdapter.java
+++ app/src/main/java/com/company/androidlist/FigureAdapter.java
@@ -1,17 +1,15 @@
 package com.company.androidlist;
 
-import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
-import android.os.Bundle;
-import android.renderscript.Sampler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -20,6 +18,7 @@ public class FigureAdapter extends RecyclerView.Adapter<FigureAdapter.ViewHolder
     private LayoutInflater layoutInflater;
     private List<Figure> figureList;
     private Context context;
+    private Dialog myDialog;
 
 
     public FigureAdapter(Context context, List<Figure> figureList) {
@@ -37,15 +36,21 @@ public class FigureAdapter extends RecyclerView.Adapter<FigureAdapter.ViewHolder
 
 
     @Override
-    public void onBindViewHolder(@NonNull FigureAdapter.ViewHolderFigure holder, int position) {
+    public void onBindViewHolder(@NonNull final FigureAdapter.ViewHolderFigure holder, int position) {
         final Figure figure = figureList.get(position);
         holder.imageViewFigure.setImageResource(figure.getImage());
         holder.nameViewFigure.setText(figure.getText());
         holder.imageViewFigure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogFigureFragment dialogFigureFragment = new DialogFigureFragment();
-                dialogFigureFragment.show(((AppCompatActivity) context).getSupportFragmentManager(), "Fragment");
+                myDialog = new Dialog(context);
+                myDialog.setContentView(R.layout.dialog_fragmet);
+                TextView figureInfo = myDialog.findViewById(R.id.figureInfoText);
+                ImageView figureImage = myDialog.findViewById(R.id.infoImage);
+                figureInfo.setText(figureList.get(holder.getAdapterPosition()).getText());
+                figureImage.setImageResource(figureList.get(holder.getAdapterPosition()).getImage());
+                myDialog.setCanceledOnTouchOutside(true);
+                myDialog.show();
             }
         });
     }
@@ -57,13 +62,15 @@ public class FigureAdapter extends RecyclerView.Adapter<FigureAdapter.ViewHolder
     }
 
     public static class ViewHolderFigure extends RecyclerView.ViewHolder {
-        final ImageView imageViewFigure;
-        final TextView nameViewFigure;
+        private ImageView imageViewFigure;
+        private TextView nameViewFigure;
+        private LinearLayout item_info;
 
         ViewHolderFigure(View view) {
             super(view);
             imageViewFigure = view.findViewById(R.id.imageFigure);
             nameViewFigure = view.findViewById(R.id.nameFigure);
+            item_info = view.findViewById(R.id.figure_item);
         }
     }
 }
